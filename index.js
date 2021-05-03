fdb3d6499a0a1f8db6eb94d32080292fbb0b2c13
GLPetroni <60444067+GLPetroni@users.noreply.github.com>
2021-05-03 10:57:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ function hide()
   }
 }
 
-
+function post_twit() 
+{
+    
+    var hide = document.getElementsByClassName("hidden");
+    for(var i = 0; i < hide.length; i++) { 
+      hide[i].style.display='none'
+  }
+}
