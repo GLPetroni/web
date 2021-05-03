7073e0c5c16c523fc945aa6c31dbcc8587ec84bb
GLPetroni <60444067+GLPetroni@users.noreply.github.com>
2021-05-03 12:23:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,24 @@ function hide()
   }
 }
 
-function post_twit() 
+function make_twit() 
 {
-    
-    var hide = document.getElementsByClassName("hidden");
-    for(var i = 0; i < hide.length; i++) { 
-      hide[i].style.display='none'
+  var content = document.getElementById("twit-text-input").value;
+  var author = document.getElementById("twit-attribgution-input").value;
+  var twit = document.createElement("ARTICLE");
+  var twit_icon = document.createElement("DIV");
+  var bullhorn = document.createElement("DIV");
+  twit_icon.appendChild(bullhorn);
+  var twit_content = document.createElement("DIV");
+  var twit_text =  document.createElement("DIV");
+  document.getElementByClassName("twit_text").value = content;
+  var twit_author = document.createElement("DIV");
+  document.getElementByClassName("twit_author").value = author;
+  document.getElementByClassName("twit_content").appendChild(twit_text);
+  document.getElementByClassName("twit_content").appendChild(twit_author);
+  document.getElementByClassName("twit-container").appendChild(twit)
+  var close = document.getElementsByClassName("hidden");
+  for(var i = 0; i < close.length; i++) { 
+    close[i].style.display='none'
   }
 }
