b14f928c42da345a193bc94be6902cd9fad0c0c2
GLPetroni <60444067+GLPetroni@users.noreply.github.com>
2021-05-03 12:34:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ function hide()
 
 function make_twit() 
 {
-//   var content = document.getElementById("twit-text-input").value;
-//   var author = document.getElementById("twit-attribgution-input").value;
-//   var twit = document.createElement("ARTICLE");
-//   var twit_icon = document.createElement("DIV");
-//   var bullhorn = document.createElement("DIV");
-//   twit_icon.appendChild(bullhorn);
-//   var twit_content = document.createElement("DIV");
-//   var twit_text =  document.createElement("DIV");
-//   document.getElementByClassName("twit_text").value = content;
-//   var twit_author = document.createElement("DIV");
-//   document.getElementByClassName("twit_author").value = author;
-//   document.getElementByClassName("twit_content").appendChild(twit_text);
-//   document.getElementByClassName("twit_content").appendChild(twit_author);
-//   document.getElementByClassName("twit-container").appendChild(twit);
+  var content = document.getElementById("twit-text-input").value;
+  var author = document.getElementById("twit-attribgution-input").value;
+  var twit = document.createElement("ARTICLE");
+  var twit_icon = document.createElement("DIV");
+  var bullhorn = document.createElement("DIV");
+  twit_icon.appendChild(bullhorn);
+  var twit_content = document.createElement("DIV");
+  var twit_text =  document.createElement("DIV");
+  document.getElementByClassName("twit_text").value = content;
+  var twit_author = document.createElement("DIV");
+  document.getElementByClassName("twit_author").value = author;
+  document.getElementByClassName("twit_content").appendChild(twit_text);
+  document.getElementByClassName("twit_content").appendChild(twit_author);
+  document.getElementByClassName("twit-container").appendChild(twit);
   hide();
 }
