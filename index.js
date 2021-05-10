93101ac3057e788209a1464624d6f4d2ec77e6ee
GLPetroni <60444067+GLPetroni@users.noreply.github.com>
2021-05-10 13:22:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function display()
 {
   var display = document.getElementsByClassName("hidden");
@@ -14,46 +15,51 @@ function hide()
   }
 }
 
-function make_twit() 
-{
-    var content = document.getElementById("twit-text-input").textContent;
-    var author = document.getElementById("twit-attribution-input").textContent;
-    var article = document.createElement("ARTICLE");
-    var div1 = document.createElement("DIV");
-    var div2 = document.createElement("I");
-    var twit_author = document.createElement("P");
-    var ref = document.createElement("A");
-    var div3 = document.createElement("DIV");
-    var div4 =  document.createElement("p");
-    
-    article.classList.add("twit");
-    div1.classList.add("twit-icon");
-    div2.classList.add("fas");
-    div2.classList.add("fa-bullhorn");
-    div3.classList.add("twit-content");
-    div4.classList.add("twit-text");
-    twit_author.classList.add("twit-author");
-    ref.setAttribute('href',"#");
-
+function not_full() {
     var tex = document.getElementById('twit-text-input').value;
     if (tex == "") {
         alert("Please fill both twit content and author to submit");
         return;
     }
-    div4.textContent = tex;
 
     var _author = document.getElementById('twit-attribution-input').value;
     if (_author == "") {
         alert("Please fill both twit content and author to submit");
         return;
     }
-    ref.textContent = _author;
+    make_twit(tex,_author);
+    var twit_obj = {text: tex, author: _author};
+    twit_arr.push(twit_obj);
+}
 
-    twit_author.appendChild(ref);
+function make_twit(twit_text, twit_author)
+{
+    var content = document.getElementById("twit-text-input").textContent;
+    var author = document.getElementById("twit-attribution-input").textContent;
+    var article = document.createElement("ARTICLE");
+    var div1 = document.createElement("DIV");
+    var div2 = document.createElement("I");
+    var twit_aut = document.createElement("P");
+    var ref = document.createElement("A");
+    var div3 = document.createElement("DIV");
+    var div4 =  document.createElement("p");
+
+    article.classList.add("twit");
+    div1.classList.add("twit-icon");
+    div2.classList.add("fas");
+    div2.classList.add("fa-bullhorn");
+    div3.classList.add("twit-content");
+    div4.classList.add("twit-text");
+    twit_aut.classList.add("twit-author");
+    ref.setAttribute('href',"#");
+    div4.textContent = twit_text;
+    ref.textContent = twit_author;
+
+    twit_aut.appendChild(ref);
     article.appendChild(div1);
     div1.appendChild(div2);
     div3.appendChild(div4);
-    div3.appendChild(twit_author);
+    div3.appendChild(twit_aut);
     article.appendChild(div3)
 
     document.querySelector('.twit-container').appendChild(article);
@@ -65,15 +71,20 @@ var search_bar = document.getElementById('navbar-search-input');
 search_bar.addEventListener('input',text_search);
 search_bar.addEventListener('input',author_search);
 var twits = document.getElementsByClassName("twit");
+var twit_arr = [];
 
-//while (twits.length > 0) {
-//    var _text = twits[0].querySelector(".twit-text").textContent;
-//    var _author = twits[0].querySelector(".twit-author").textContent;
-//    var twit_obj = {text: _text, author: _author};
-//    var twit_arr = [];
-//    twit_arr.push(twit_obj);
-//    console.log(twit_arr);
-//    }
+while (twits.length > 0) {
+    var text = twits[0].querySelector(".twit-text").textContent;
+    var author = twits[0].querySelector(".twit-author").textContent;
+    var twit_obj = {text: text, author: author};
+    twit_arr.push(twit_obj);
+    console.log(twit_arr);
+    twits[0].remove();
+    }
+
+twit_arr.forEach(function(twit) {
+    make_twit(twit.text,twit.author);
+});
 function author_search() {
     var input = search_bar.value;
     var filter = input.toUpperCase();
